896347efc89f5b0e696e2184ad8807c7a84a8268
Ross Addison <rossaddison@users.noreply.github.com>
2023-12-06 18:28:23 +0000

    Installation Testing 2: remove /blog from .env.example file

--- resources/rbac/assignments.php
+++ resources/rbac/assignments.php
@@ -1,3 +1,15 @@
 <?php
-
-return [];
+return [
+    1 => [
+        'admin',
+    ],
+    2 => [
+        'observer',
+    ],
+    3 => [
+        'observer',
+    ],
+    4 => [
+        'observer',
+    ],
+];
