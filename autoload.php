dbc11d3fd3c9a603b1a71ce1164e388f937a51a8
Ross Addison <rossaddison@users.noreply.github.com>
2024-08-26 18:39:05 +0100

    Super Global Test Static Analysis: Ubuntu 8.2 latest Test 4
    Restore original values.

--- autoload.php
+++ autoload.php
@@ -9,27 +9,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-/**
- * Avoiding Netbeans message: Do not access Super Globals directly
- * If you are on another development platform you may prefer autoload_wo_filter_input.php
- * FILTER_DEFAULT implies no filtering of string is happening according to e.g. FILTER_SANITIZE_SPECIAL_CHARS
- * e.g $_ENV['YII_DEBUG'] represented as filter_input(INPUT_ENV, 'YII_DEBUG', FILTER_DEFAULT)
- * e.g $_SERVER['YII_DEBUG'] represented as filter_input(INPUT_SERVER, 'YII_DEBUG', FILTER_DEFAULT)
- * Possible return values of filter_input: null if variable not set, false if filter fails, and the value of the variable if it passes
- * @see {root} .env file for environment variables
- * @see https://www.php.net/manual/en/function.filter-input.php
- * @see https://stackoverflow.com/questions/19767894/warning-do-not-access-superglobal-post-array-directly-on-netbeans-7-4-for-ph
- */
+$_ENV['YII_ENV'] = strlen(($_ENV['YII_ENV'])) == 0 ? null : $_ENV['YII_ENV'];
+$_SERVER['YII_ENV'] = $_ENV['YII_ENV'];
 
-$filterYiiEnv = filter_input(INPUT_ENV, 'YII_ENV', FILTER_DEFAULT);
-$yiiEnv = (null!==$filterYiiEnv && $filterYiiEnv !== false) ? $filterYiiEnv : '';
-$_SERVER['YII_ENV'] = $yiiEnv;
-
-$filterYiiDebug = filter_input(INPUT_ENV, 'YII_DEBUG', FILTER_DEFAULT);
-$yiiDebug = filter_var(
-    // $filterYiiDebug: variable has been set (i.e. variable not null). If the filter fails allow $filterYiiDebug to be false.      
-    null!==$filterYiiDebug ? $filterYiiDebug : true,
+$_ENV['YII_DEBUG'] = filter_var(
+    !strlen($_ENV['YII_DEBUG']) == 0 ? $_ENV['YII_DEBUG'] : true,
     FILTER_VALIDATE_BOOLEAN,
     FILTER_NULL_ON_FAILURE
 ) ?? true;
-$_SERVER['YII_DEBUG'] = $yiiDebug;
+$_SERVER['YII_DEBUG'] = $_ENV['YII_DEBUG'];
--- public/index.php
+++ public/index.php
@@ -4,18 +4,24 @@ declare(strict_types=1);
 
 use Yiisoft\Yii\Runner\Http\HttpApplicationRunner;
 
-if (is_bool(getenv('YII_C3'))) {
+/**
+ * @psalm-suppress RiskyTruthyFalsyComparison getenv('YII_C3')
+ */
+if (getenv('YII_C3')) {
     $c3 = dirname(__DIR__) . '/c3.php';
     if (file_exists($c3)) {
         require_once $c3;
     }
 }
 
-    // PHP built-in server routing.
+/**
+ * @psalm-var string $_SERVER['REQUEST_URI']
+ */
+// PHP built-in server routing.
 if (PHP_SAPI === 'cli-server') {
     // Serve static files as is.
     /** @psalm-suppress MixedArgument */
-    $path = parse_url(filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_DEFAULT) ?? '', PHP_URL_PATH);
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     if (is_file(__DIR__ . $path)) {
         return false;
     }
@@ -27,29 +33,11 @@ if (PHP_SAPI === 'cli-server') {
 chdir(dirname(__DIR__));
 require_once dirname(__DIR__) . '/autoload.php';
 
-/**
- * Avoiding Netbeans message: Do not access Super Globals directly
- * FILTER_DEFAULT implies no filtering of string is happening according to e.g. FILTER_SANITIZE_SPECIAL_CHARS
- * e.g $_ENV['YII_DEBUG'] represented as filter_input(INPUT_ENV, 'YII_DEBUG', FILTER_DEFAULT)
- * Possible return values of filter_input: null if variable not set, false if filter fails, and the value of the variable if it passes
- * @see {root} .env file for environment variables
- * @see https://www.php.net/manual/en/function.filter-input.php
- * @see https://stackoverflow.com/questions/19767894/warning-do-not-access-superglobal-post-array-directly-on-netbeans-7-4-for-ph
- */
-
-$filterYiiEnv = filter_input(INPUT_ENV, 'YII_ENV', FILTER_DEFAULT);
-
-// if the variable has been set (i.e variable not null) and the filter has not failed (not false)
-$yiiEnv = (null!==$filterYiiEnv && $filterYiiEnv !== false) ? $filterYiiEnv : '';
-
 // Run HTTP application runner
 $runner = new HttpApplicationRunner(
     rootPath: dirname(__DIR__),
-    // e.g. true or false    
-    debug: $_ENV['YII_DEBUG'],              
-    // e.g. true or false    
+    debug: $_ENV['YII_DEBUG'],
     checkEvents: $_ENV['YII_DEBUG'],
-    // e.g. 'prod' or 'dev' or ''    
-    environment: $yiiEnv  
+    environment: $_ENV['YII_ENV']
 );
 $runner->run();
