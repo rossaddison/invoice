9de12ad425754f73618f0f4a24fa08b704858b1b
Ross Addison <rossaddison@users.noreply.github.com>
2024-09-25 12:55:05 +0100

    CORS Preparation for Dev Panel Usage
    Information Update

---
+++ resources/views/invoice/info/cors.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+?>
+
+<p><b>How do I prepare CORS (Cross-Origin-Resource-Sharing) on Chrome so that I can access the Yiisoft Dev Panel from my localhost?</b></p>
+
+<p><b><a href="https://stackoverflow.com/questions/43262121/trying-to-use-fetch-and-pass-in-mode-no-cors">Step 1: Read: ...If you are trying to address this issue temporarily on your localhost, you can use this Chrome extension :</a></b><br>
+<p><b><a href="https://webbrowsertools.com/test-cors/">Step 2: Test CORS</a></b><br>    
+<p><b><a href="chrome-extension://lhobafahddgcelffkeicbaginigeejlf/data/options/options.html">Step 3: Install Chrome CORS Extension</a></b><br>
