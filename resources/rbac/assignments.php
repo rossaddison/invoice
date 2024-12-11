5e3d21b693fc1d9292e38a963d76af1cffeb506d
Ross Addison <rossaddison@users.noreply.github.com>
2024-12-11 02:48:13 +0000

    update composer

--- resources/rbac/assignments.php
+++ resources/rbac/assignments.php
@@ -1,3 +1,12 @@
 <?php
 
-return [];
+return [
+    1 => [
+        'user_id' => '1',
+        'item_name' => 'admin',
+    ],
+    2 => [
+        'user_id' => '2',
+        'item_name' => 'observer',
+    ],
+];
