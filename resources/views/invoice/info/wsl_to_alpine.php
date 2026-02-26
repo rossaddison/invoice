3d72cfab1f838084d8f39ecd702ea430791d60ec
Ross Addison <rossaddison@users.noreply.github.com>
2026-02-26 17:43:21 +0000

    wsl to alpine: update useful commands
    Index page was not accepting css from BootstrapCdn:
    In the Dom, I was getting (index):11 Loading the stylesheet 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css' violates the following Content Security Policy directive: "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com". Note that 'style-src-elem' was not explicitly set, so 'style-src' is used as a fallback. The action has been blocked.
    
    So I had to modify the .htaccess 'Header always set line ...' with the following:
    
    Header always set Content-Security-Policy "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://cdn.jsdelivr.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; font-src 'self' https://fonts.gstatic.com https://cdn.jsdelivr.net; img-src 'self' data: blob: https:; connect-src 'self' https://api.storecove.com https://api.stripe.com; manifest-src 'self'; worker-src 'self'"
    
    And then service apache2 restart.
    
    The config/common/params.php is included in the .gitignore so after 'git pull origin main' , the params.php had to be copied and pasted over, along with the .env file.

--- resources/views/invoice/info/wsl_to_alpine.php
+++ resources/views/invoice/info/wsl_to_alpine.php
@@ -21,7 +21,7 @@ declare(strict_types=1);
          cd var/www/invoice
  <b>5. Verify git is installed before using it:</b>
          git -- version
- <b>6. Upgrade git:</b> 
+ <b>6. Upgrade git:</b>
          apk update && apk upgrade git
  <b>7. Check for any local changes that you have made on the website before pulling:</b>
          git status
@@ -32,5 +32,24 @@ declare(strict_types=1);
  <b>10. Restoring a specific file from the stash:</b>
          git checkout stash@{0} -- config/common/params.php
  <b>11. Pull from the repository:
-         git pull origin main</b></p>
+         git pull origin main
+ <b>12. Accessing the Apline mySql/mariaDB database:
+         mysql -u root -p</b>
+ <b>13. Listing Multiple Stashes:
+         git stash list</b>
+ <b>14. Getting out of the stash list whilst viewing it by pressing q:
+         q</b>
+ <b>15. See what is in your stash without applying it:
+         git stash show -p</b>
+ <b>16. Giving ownership to apache user:
+         chown -R apache:apache /var/www/invoice/
+         chmod -R 755 /var/www/invoice/
+         chmod -R 775 /var/www/invoice/resources/
+         chmod -R 775 /var/www/invoice/runtime/
+         chmod -R 775 /var/www/invoice/public/assets/</b>
+ <b>17. Ensure that the resources/rbac/items.php file, when logged in
+        as the admin, is rotating between a visible 'entry.to.base.controller'
+        and invisible.
+ </b>
+</p>
 </pre>
